685dfc92575b555ad505087d2a8f0b02b0e5e835
Lakshman Chelliah <lakshman.chelliah@gmail.com>
2023-03-08 19:12:43 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Python Blackjack Game
+This is a Python implementation of the classic card game Blackjack, built in VScode using the Pygame, Random, and Math Python libraries.
+
+# Game Rules
+The objective of Blackjack is to have a hand value of 21 or as close to 21 as possible without going over. Each card in the deck has a value. Number cards are worth their face value, face cards (Kings, Queens, and Jacks) are worth 10, and Aces can be worth 1 or 11, depending on which value would be more beneficial to the player.
+
+At the start of the game, each player (including the dealer) is dealt two cards. The player's cards are dealt face up, while one of the dealer's cards is dealt face down (known as the hole card).
+
+Players then have the option to hit (take another card), stand (keep their current hand value), double down (double their bet and receive one more card), or split (if they have a pair of cards with the same value, they can split them into two separate hands).
+
+The dealer must hit until they have a hand value of 17 or higher. If the dealer's hand value exceeds 21, they bust and all remaining players win.
+
+# Features
+- Multi Deck Shoe: The game uses multiple decks of cards, which are shuffled together at the start of each round.
+- Double Down Functionality: Players can choose to double their bet and receive one more card.
+- Multi Ace auto value: The game automatically assigns the best value for Aces in a player's hand (either 1 or 11).
+- Dealer hits on x: The dealer must hit until they have a hand value of 17 or higher.
+# Dependencies
+Pygame
+Random
+Math
+# How to Play
+To run the game, run the blackjack.py file in your Python environment.
+
+Once the game is started, follow the on-screen instructions to play.
+
+Good luck, and have fun!
+
+# Change Log
+- 0.0 -> Initial Release [@LakshmanChelliah](https://github.com/LakshmanChelliah)
+- 0.1 -> Bug Fixes and Error Handling [@DylanDominic](https://github.com/DylanD2402)
---
+++ blackjackgame.py
@@ -0,0 +1,211 @@
+
+import random
+import math
+
+
+SHOE_SIZE = 6
+DEALER_HIT_ON = 16
+
+#Creates BlackJack Shoe with SHOE_SIZE amount of decks 
+def makeShoe()->list:
+    deck = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+    symbols = ["♠","♥","♦","♣"]
+    
+    finaldeck = [];
+
+    for x in symbols:
+        for y in deck:
+            finaldeck.append(str(x) + str(y))
+    finaldeck = finaldeck*SHOE_SIZE
+    random.shuffle(finaldeck)
+    
+    return finaldeck
+
+
+#Converts Cards in Hand to a Value to be compared with opponent 
+#Looks at Number of "A" and determines when the best value with n Aces
+def checkScore(hand) -> int:
+    score = 0
+    aceCount = 0
+    
+    for card in hand:
+        temp = card[-1]
+        if (temp == "J" or temp == "Q" or temp == "K"):
+            score+=10
+        elif (temp != "A"):
+            if (temp=="0"):
+                score +=10
+            else:
+                score = score + int(temp)
+        else:
+            score+=11
+            aceCount += 1
+            
+            
+    while score > 21 and aceCount > 0:
+        score -= 10
+        aceCount -= 1
+            
+    return score
+    
+    
+#Made for Player
+#Gives another card until not needed
+def hitCard (hand, deck)->bool:
+    hand.append(deck.pop())
+    print("Player Cards")
+    print(hand)
+    while True:
+        if (checkScore(hand) > 21):
+            return False
+        else:
+            userIn = input('What would you like to do?\n-\'H\'it -\'S\'tand   : ')
+            if (userIn.upper() == 'H'):
+                return True
+            elif (userIn.upper() == 'S'):
+                return False
+            else:
+                print("Invalid Input")
+                userIn = input('What would you like to do?\n-\'H\'it -\'S\'tand   : ')
+
+
+#Dealer's Version of hitCard
+#Dealer will hit on DEALER_HIT_ON
+def dealerTurn(hand, deck)->None:
+    
+    print("Dealers Cards")
+    print(hand)
+    
+    while (checkScore(hand) < DEALER_HIT_ON):
+        print("Dealer HIT")
+        hand.append(deck.pop())
+        print(hand)
+      
+        
+#Evalutes round and returns:
+#1 for Player 
+#-1 for Dealer
+#0 for Tie
+#2 for Blackjack
+def evalRound(player, dealer)->int:
+    
+    playerBust = checkScore(player) > 21
+    dealerBust = checkScore(dealer) > 21
+    
+    print('\n')
+    if (dealerBust and not playerBust):
+        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+        print("DEALER BUST")
+        print("YOU WON")
+        return 1
+    elif (playerBust and not dealerBust):
+        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+        print("YOU BUST")
+        print("YOU LOST")
+        return -1
+    elif (playerBust and dealerBust):
+        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+        print("BOTH BUST")
+        print("PUSH")
+        return 0
+    elif (checkScore(player) > checkScore(dealer)):
+        if (len(player)==2 and checkScore(player)==21):
+            print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+            print("YOU WON w/ BLACKJACK")            
+            return 2
+        else:
+            print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+            print("YOU WON")
+            return 1
+    elif (checkScore(player) < checkScore(dealer)):
+        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+        print("YOU LOST")
+        return -1   
+    elif (checkScore(player) == checkScore(dealer)):
+        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+        print("PUSH")
+        return 0     
+    
+
+#Plays out a Single Round    
+def startRound(bet, balance, deck)->int:
+    
+    dealerhand = [deck.pop(),deck.pop()] 
+    
+    playerhand = [deck.pop(),deck.pop()]
+    print("Your Hand:")
+    print(playerhand)
+    print("\nDealer's Up Card:")
+    print("["+dealerhand[0]+"]\n")
+    
+   
+    while True:
+        userIn = input('What would you like to do?\n-\'H\'it -\'S\'tand -\'D\'ouble   : ')
+        if (userIn.upper() == 'H'):
+            while (hitCard(playerhand,deck)):
+                continue
+            break
+        elif(userIn.upper() == 'S'):
+            break
+        elif(userIn.upper() == 'D'):
+            if (bet*2 > balance):
+                print("You don't have enough money to double")
+                continue
+            else:
+                playerhand.append(deck.pop())
+                print("Player Cards")
+                print(playerhand)                
+                
+                bet = bet + bet
+                break
+        else:
+            print("Invalid Input")
+            
+
+            
+    dealerTurn(dealerhand,deck)  
+    
+    result = (evalRound(playerhand,dealerhand))
+    
+    if result==1: 
+        balance += bet
+    elif result==-1:
+        balance -= bet
+    elif result ==2:
+        balance += math.ceil(bet * 1.5)
+    
+    print("New Balance: " + str(balance) + "\n")
+    return balance
+    
+#Plays the game till User runs out of money   
+def startGame(balance)->None:
+    gameDeck = makeShoe()
+    while (balance > 0):
+        bet = input('How much would you like to bet?   : ')
+        try:
+            bet = int(bet)
+
+        except:
+            print("Invalid Input")
+            continue
+        if (bet>balance):
+            print("Not enough money!")
+        else:
+            balance = startRound(bet,balance,gameDeck)        
+            
+    
+if __name__ == '__main__':
+
+    while True: 
+        balance = input('How much would you like to play with today?   : ')
+
+        try: 
+            balance = int(balance)
+            startGame(balance)
+            break
+        except:
+            print("Invalid Input")
+
+print("Game Over")
+    
+    
