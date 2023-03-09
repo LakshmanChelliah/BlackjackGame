1d570fd8a356d54cdae0e12698e406752a29cd4d
Lakshman Chelliah <lakshman.chelliah@gmail.com>
2023-03-08 20:56:27 -0500

    Reupload 0.0.2

--- blackjackgame.py
+++ blackjackgame.py
@@ -2,12 +2,15 @@
 import random
 import math
 import time
+'''
+Authors: Lakshman Chelliah, Dylan Dominic
+Version 0.0.2
+Date: 2023-03-08
+'''
 
-# Authors: Lakshman Chelliah, Dylan Dominic
-# Version 0.0.1
-# Date: 2023-03-08
-
+#How many Decks there are
 SHOE_SIZE = 6
+#Max number the dealer will hit on
 DEALER_HIT_ON = 16
 
 #Creates BlackJack Shoe with SHOE_SIZE amount of decks 
@@ -20,6 +23,7 @@ def makeShoe()->list:
     for x in symbols:
         for y in deck:
             finaldeck.append(str(x) + str(y))
+    #Creates *SHOE_SIZE* amount of decks
     finaldeck = finaldeck*SHOE_SIZE
     random.shuffle(finaldeck)
     
@@ -33,6 +37,7 @@ def checkScore(hand) -> int:
     aceCount = 0
     
     for card in hand:
+        #Takes the last character of the card and determines if its value
         temp = card[-1]
         if (temp == "J" or temp == "Q" or temp == "K"):
             score+=10
@@ -45,7 +50,8 @@ def checkScore(hand) -> int:
             score+=11
             aceCount += 1
             
-            
+    #If the value is greater than 21 and there are aces present
+    #subtract 10 from the count to make the aces represent 1 instead of 11        
     while score > 21 and aceCount > 0:
         score -= 10
         aceCount -= 1
@@ -56,9 +62,12 @@ def checkScore(hand) -> int:
 #Made for Player
 #Gives another card until not needed
 def hitCard (hand, deck)->bool:
+    #Initial HIT
     hand.append(deck.pop())
     print("Player Cards")
     print(str(hand) + " -> " + str(checkScore(hand)))
+
+    #Lets player keep HITting until they STAND
     while True:
         if (checkScore(hand) > 21):
             return False
@@ -95,56 +104,81 @@ def dealerTurn(hand, deck)->None:
 #2 for Blackjack
 def evalRound(player, dealer)->int:
     
+    #Check if player or dealer busts
     playerBust = checkScore(player) > 21
     dealerBust = checkScore(dealer) > 21
+
+    playerCount = checkScore(player) 
+    dealerCount = checkScore(dealer)
     
     print('\n')
+    #If Dealer busts
     if (dealerBust and not playerBust):
-        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+        print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
         print("DEALER BUST")
         print("YOU WON")
         return 1
+    #If Player busts
     elif (playerBust and not dealerBust):
-        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+        print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
         print("YOU BUST")
         print("YOU LOST")
         return -1
+    #If both busts
     elif (playerBust and dealerBust):
-        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+        print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
         print("BOTH BUST")
         print("PUSH")
         return 0
-    elif (checkScore(player) > checkScore(dealer)):
-        if (len(player)==2 and checkScore(player)==21):
-            print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+    #If Plaver gets higher
+    elif (playerCount > dealerCount):
+        if (len(player)==2 and playerCount==21):
+            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
             print("YOU WON w/ BLACKJACK")            
             return 2
         else:
-            print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
             print("YOU WON")
             return 1
-    elif (checkScore(player) < checkScore(dealer)):
-        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
+    #If Dealer gets higher
+    elif (playerCount < dealerCount):
+        print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
         print("YOU LOST")
         return -1   
-    elif (checkScore(player) == checkScore(dealer)):
-        print("Player: " + str(checkScore(player)) + "\nDealer: " + str(checkScore(dealer)))
-        print("PUSH")
-        return 0     
+   #If they are equal
+    elif (playerCount == checkScore(dealer)):
+        #If Player had Blackjack
+        if (len(player)==2 and playerCount==21) and len(dealer)>2:
+            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
+            print("YOU WON w/ BLACKJACK")            
+            return 2
+        #If Dealer had Blackjack
+        elif (len(player)>2 and playerCount==21) and len(dealer)==2:
+            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
+            print("YOU LOST, DEALER HAD BLACKJACK")    
+            return -1
+        #Else they are just a tie
+        else: 
+            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
+            print("PUSH")
+            return 0     
     
 
 #Plays out a Single Round    
 def startRound(bet, balance, deck)->int:
-    
+    #Give 2 Cards to Dealer
     dealerhand = [deck.pop(),deck.pop()] 
-    
+    #Give 2 Cards to Player
     playerhand = [deck.pop(),deck.pop()]
+
     print("Your Hand:")
     print(str(playerhand) + " -> " + str(checkScore(playerhand)))
-    print("\nDealer's Up Card:")
+    print("\nDealer's Up Card:")\
+    
+    #Show Dealer's Up Card
     print("["+dealerhand[0]+"]\n")
     
-   
+   #Inital Options for Player (Includes Double)
     while True:
         userIn = input('What would you like to do?\n-\'H\'it -\'S\'tand -\'D\'ouble   : ')
         if (userIn.upper() == 'H'):
@@ -171,12 +205,16 @@ def startRound(bet, balance, deck)->int:
             
     dealerTurn(dealerhand,deck)  
     
+    #Checks the result of the game
     result = (evalRound(playerhand,dealerhand))
     
+    #Player Wins
     if result==1: 
         balance += bet
+    #Dealer Wins
     elif result==-1:
         balance -= bet
+    #Player Wins from Blackjack
     elif result ==2:
         balance += math.ceil(bet * 1.5)
     
@@ -186,6 +224,7 @@ def startRound(bet, balance, deck)->int:
 #Plays the game till User runs out of money   
 def startGame(balance)->None:
     gameDeck = makeShoe()
+    #Game goes until the user runs out of money
     while (balance > 0):
         bet = input('How much would you like to bet?   : ')
         try:
@@ -197,11 +236,13 @@ def startGame(balance)->None:
         if (bet>balance):
             print("Not enough money!")
         else:
+            #Balance gets updated each round
             balance = startRound(bet,balance,gameDeck)        
             
     
 if __name__ == '__main__':
 
+    #Takes intial balance
     while True: 
         balance = input('How much would you like to play with today?   : ')
 
