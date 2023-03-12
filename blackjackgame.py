32c9a2732d1a2ac53223cc753124320df117f554
Dylan Dominic <dylanrishid@gmail.com>
2023-03-12 17:33:51 -0400

    Colours update!

--- blackjackgame.py
+++ blackjackgame.py
@@ -16,10 +16,15 @@ import sys
 class colors:
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
-    RED = '\033[91m'
+    REDB = '\033[1;91m'
+    REDBB = '\033[38;5;124m'
+    RED = '\033[0;31m'
+    CYAN = '\033[0;36m'
+    CYANB = '\033[1;96m'
+    GREENB = '\033[48;5;22m'
     END = '\033[0m'
 
-
+#colors.RED + "Invalid Input" + colors.END
 
 #How many Decks there are
 SHOE_SIZE = 6
@@ -77,8 +82,8 @@ def checkScore(hand) -> int:
 def hitCard (hand, deck)->bool:
     #Initial HIT
     hand.append(deck.pop())
-    print("Player Cards")
-    print(str(hand) + " -> " + str(checkScore(hand)))
+    print(colors.CYANB+"Player Cards"+colors.END)
+    print(colors.CYAN+str(hand) + " -> " + str(checkScore(hand))+colors.END)
 
     #Lets player keep HITting until they STAND
     while True:
@@ -91,7 +96,7 @@ def hitCard (hand, deck)->bool:
             elif (userIn.upper() == 'S'):
                 return False
             else:
-                print("Invalid Input")
+                print(colors.REDBB+"Invalid Input"+colors.END)
                 userIn = input('What would you like to do?\n-\'H\'it -\'S\'tand   : ')
 
 
@@ -99,11 +104,11 @@ def hitCard (hand, deck)->bool:
 #Dealer will hit on DEALER_HIT_ON
 def dealerTurn(hand, deck)->None:
     
-    print("Dealers Cards")
+    print(colors.REDB+"Dealers Cards"+ colors.END)
     print(hand)
     
     while (checkScore(hand) <= DEALER_HIT_ON):
-        print("Dealer HIT")
+        print(colors.REDB+"Dealer HIT"+ colors.END)
         time.sleep(3)
         hand.append(deck.pop())
         print(hand)
@@ -127,52 +132,52 @@ def evalRound(player, dealer)->int:
     print('\n')
     #If Dealer busts
     if (dealerBust and not playerBust):
-        print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
-        print("DEALER BUST")
-        print("YOU WON")
+        print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
+        print(colors.GREENB+"DEALER BUST"+ colors.END)
+        print(colors.GREENB+"YOU WON"+colors.END)
         return 1
     #If Player busts
     elif (playerBust and not dealerBust):
-        print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
-        print("YOU BUST")
-        print("YOU LOST")
+        print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
+        print(colors.REDBB+"YOU BUST"+colors.END)
+        print(colors.REDBB+"YOU LOST"+colors.END)
         return -1
     #If both busts
     elif (playerBust and dealerBust):
-        print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
+        print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
         print("BOTH BUST")
         print("PUSH")
         return 0
     #If Plaver gets higher
     elif (playerCount > dealerCount):
         if (len(player)==2 and playerCount==21):
-            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
-            print("YOU WON w/ BLACKJACK")            
+            print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
+            print(colors.GREENB+"YOU WON w/ BLACKJACK"+colors.END)            
             return 2
         else:
-            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
-            print("YOU WON")
+            print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
+            print(colors.GREENB+"YOU WON"+colors.END)
             return 1
     #If Dealer gets higher
     elif (playerCount < dealerCount):
-        print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
-        print("YOU LOST")
+        print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
+        print(colors.REDBB+"YOU LOST"+colors.END)
         return -1   
    #If they are equal
     elif (playerCount == checkScore(dealer)):
         #If Player had Blackjack
         if (len(player)==2 and playerCount==21) and len(dealer)>2:
-            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
-            print("YOU WON w/ BLACKJACK")            
+            print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
+            print(colors.GREENB+"YOU WON w/ BLACKJACK"+colors.END)            
             return 2
         #If Dealer had Blackjack
         elif (len(player)>2 and playerCount==21) and len(dealer)==2:
-            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
-            print("YOU LOST, DEALER HAD BLACKJACK")    
+            print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
+            print(colors.REDBB+"YOU LOST, DEALER HAD BLACKJACK"+colors.END)    
             return -1
         #Else they are just a tie
         else: 
-            print("Player: " + str(playerCount) + "\nDealer: " + str(dealerCount))
+            print(colors.CYANB+"Player: "+colors.END + str(playerCount) + colors.REDB+"\nDealer: "+colors.END + str(dealerCount))
             print("PUSH")
             return 0     
     
@@ -184,12 +189,12 @@ def startRound(bet, balance, deck)->int:
     #Give 2 Cards to Player
     playerhand = [deck.pop(),deck.pop()]
 
-    print("Your Hand:")
-    print(str(playerhand) + " -> " + str(checkScore(playerhand)))
-    print("\nDealer's Up Card:")
+    print(colors.CYANB+"Your Hand:"+colors.END)
+    print(colors.CYAN + str(playerhand) + " -> " + str(checkScore(playerhand))+ colors.END)
+    print(colors.REDB+"\nDealer's Up Card:"+colors.END)
     
     #Show Dealer's Up Card
-    print("["+dealerhand[0]+"]\n")
+    print(colors.RED + "["+dealerhand[0]+"]\n"+ colors.END)
     
    #Inital Options for Player (Includes Double)
     while True:
@@ -206,13 +211,13 @@ def startRound(bet, balance, deck)->int:
                 continue
             else:
                 playerhand.append(deck.pop())
-                print("Player Cards")
+                print(colors.CYANB+"Player Cards"+colors.END)
                 print(str(playerhand) + " -> " + str(checkScore(playerhand)))              
                 
                 bet = bet + bet
                 break
         else:
-            print(colors.RED + "Invalid Input" + colors.END)
+            print(colors.REDBB + "Invalid Input" + colors.END)
             
 
             
@@ -244,7 +249,7 @@ def startGame(balance)->None:
             bet = int(bet)
 
         except:
-            print(colors.RED + "Invalid Input" + colors.END)
+            print(colors.REDBB + "Invalid Input" + colors.END)
             continue
         if (bet>balance):
             print("Not enough money!")
@@ -262,9 +267,9 @@ if __name__ == '__main__':
         try: 
             balance = int(balance)
         except:
-            print(colors.RED + "Invalid Input: balance must be a number " + colors.END)
+            print(colors.REDBB + "Invalid Input: balance must be a number " + colors.END)
             continue
             
         startGame(balance)
-
-print(colors.RED + "Game Over" + colors.END)
+#has to be run as a script  
+print(colors.REDB + "Game Over" + colors.END)
